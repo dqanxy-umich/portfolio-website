c44119a9da8ced1a7359fa22f3c6928d5f94dcd1
dqanxy-umich <83249976+dqanxy@users.noreply.github.com>
2024-02-29 19:29:12 -0500

    Initial commit

---
+++ src/smstates/IdleState.tsx
@@ -0,0 +1,12 @@
+import Starmap from "../Starmap";
+import BaseState from "./BaseState";
+
+//Not react components.
+export default  class IdleState extends BaseState{
+
+    constructor(){
+        super();
+    }
+    start(){}
+    update(){}
+}
---
+++ src/smstates/StateTemplate.tsx
@@ -0,0 +1,12 @@
+import BaseState from "./BaseState";
+import Starmap from "../Starmap";
+
+//Not react components.
+export default  class StateTemplate extends BaseState{
+
+    constructor(){
+        super();
+    }
+    start(){}
+    update(){}
+}
