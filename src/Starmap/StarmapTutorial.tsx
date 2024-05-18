d5023dacf629cacec8fcad05f7d78f0f654ca6d6
dqanxy-umich <dqanxy@umich.edu>
2024-05-18 05:42:40 -0400

    basic tutorial boxes

--- src/Starmap/StarmapTutorial.tsx
+++ src/Starmap/StarmapTutorial.tsx
@@ -1,3 +1,4 @@
+import { Center } from "@chakra-ui/react";
 import TutorialState from "../smstates/TutorialState"
 import '../Starmap.css';
 
@@ -6,7 +7,40 @@ interface ISTutorial {
 }
 
 export default function StarmapTutorial(props:ISTutorial) {
-    return (
-            <div className="tutorial-box"></div>
+    let box1 = 
+    <div className="fade-in-anim">
+        <div className="tutorial-box">
+            <p className="text-header" style={{"margin":0, "textAlign":"left", "width":"100%", "height":34}}> 
+                Welcome...
+            </p>
+            <p className="text-standard" style={{"margin":0, "textAlign":"left", "width":"100%"}}>
+                Please select a star to travel there.
+                </p>
+        </div>
+    </div>
+    let box2 = 
+    <div className="fade-in-anim">
+        <div className="tutorial-box">
+            <p className="text-header" style={{"margin":0, "textAlign":"left", "width":"100%", "height":34}}> 
+                Every star is an experience...
+            </p>
+            <p className="text-standard" style={{"margin":0, "textAlign":"left", "width":"100%"}}>
+                Each experience has a description, which you can read down below.
+                </p>
+        </div>
+    </div>
+    let box3 = 
+    <div className="fade-in-anim">
+        <div className="tutorial-box">
+            <p className="text-header" style={{"margin":0, "textAlign":"left", "width":"100%", "height":34}}> 
+                Click other stars to explore...
+            </p>
+            <hr className="tutorial-line"/>
+            <p className="text-standard" style={{"margin":0, "textAlign":"left", "width":"100%"}}>
+                There's also some links to important stars at the bottom of the page. There's about a hundred stars in this constellation, have fun!
+            </p>
+        </div>
+    </div>
+    return (box3
     )
 }
